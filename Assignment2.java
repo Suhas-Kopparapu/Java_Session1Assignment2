9f0baac1ce214ee211aad1140746af9db72fd125
Suhas-Kopparapu <kopparapu.suhas@gmail.com>
2016-08-17 01:42:08 +0530

    Session1Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class Assignment2 {
+	public static  void main(String[] args){
+		//declaring three int variables a,b,c and initializing b to 4
+		int a,b=4,c;
+		//checking b value
+		System.out.println(b);
+		
+	}
+
+}
